560916058252e3f1250493e26d0dcd490334a3d0
sureshnath <sureshnath@gmail.com>
2019-05-01 17:24:20 +0100

    utility functions to set responses
    offers few methods most used setEnvVar

---
+++ postman-set-response-as-env-var.js
@@ -0,0 +1,24 @@
+var isPlainObject = function (obj) {
+	return Object.prototype.toString.call(obj) === '[object Object]';
+};
+
+var isNotObjectOrArray = function (obj) {
+	return ! (isPlainObject(obj) && Array.isArray(obj));
+};
+
+var jsonData = JSON.parse(responseBody);
+
+function setEnvVar(prefix, data){
+  for (var prop in jsonData){
+      if (isNotObjectOrArray(prop)){
+          pm.environment.set(prefix+prop, data[prop]);
+          log.debug("Setting env variable - " + prefix.prop + " = " + data[prop]);
+      }else{
+          log.info("Skipping response property - " + prefix.prop );
+      }
+  }
+}
+
+function setEnvVar(){
+  setEnvVar("resp.", jsonData);
+}
